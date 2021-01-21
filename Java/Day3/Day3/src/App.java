ecf437cae7b8a4c89d675e8b8c5268e0e0e41844
JohnnyJii <jukka.juntunen@metropolia.fi>
2021-01-21 12:10:15 +0200

    task: akata day3

--- Java/Day3/Day3/src/App.java
+++ Java/Day3/Day3/src/App.java
@@ -1,5 +1,19 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class PascalCase {
+    public static void main(String[] args) {
+        String a1 = "pineapple on pizzas";
+        String a2 = "the lazy dog jumps over bridge";
+        String a3 = "The qUick!  bRoWn fox    jumped, OVER the    lazy. dog";
+
+        System.out.println(ConvertPascal(a1));
+        System.out.println(ConvertPascal(a2));
+        System.out.println(ConvertPascal(a3));
     }
-}
+      private static String ConvertPascal(String a) {
+          String[] parts = a.split(" +");
+           for(int i = 0; 1< parts.length; i++) {
+            String p = parts[i].toLowerCase().replaceAll(",.?!", "");
+            parts[i] = p.substring(0, 1).toUpperCase() + p.substring(i);
+           }
+          return String.join("", parts);
+      }
+    }
